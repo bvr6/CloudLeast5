6f4f4d2bdc39d5cfe19fd264b357a222325f2a0a
Russ <bvr6@pitt.edu>
2020-11-09 20:42:18 -0500

    Added treemap to Reducer

--- src/CloudLeast5Reducer.java
+++ src/CloudLeast5Reducer.java
@@ -1,17 +1,19 @@
 import java.io.*; 
 import org.apache.hadoop.io.Text; 
 import org.apache.hadoop.io.LongWritable; 
-import org.apache.hadoop.mapreduce.Reducer; 
+import org.apache.hadoop.mapreduce.Reducer;
+import java.util.Map;
+import java.util.TreeMap; 
   
 public class CloudLeast5Reducer extends Reducer<Text,LongWritable,Text,LongWritable> {
     private LongWritable result = new LongWritable();
     static int count;
-    
+    private TreeMap<Integer, String> tmap;
     @Override
     public void setup(Context context) throws IOException, 
                                      InterruptedException 
     { 
-        count = 0; 
+        tmap = new TreeMap<Integer, String>(); 
     }
     
     public void reduce(Text key, Iterable<LongWritable> values,
@@ -19,13 +21,26 @@ public class CloudLeast5Reducer extends Reducer<Text,LongWritable,Text,LongWrita
                        ) throws IOException, InterruptedException {
       int sum = 0;
       for (LongWritable val : values) {
-        sum -= val.get();
+        sum += val.get();
       }
       result.set(sum);
-      if (count < 5) 
+      tmap.put(-sum, key.toString());
+      if (tmap.size() > 5) { 
+    	  tmap.remove(tmap.firstKey()); 
+      }
+    }
+    
+    @Override
+    public void cleanup(Context context) throws IOException, 
+                                       InterruptedException 
+    { 
+  
+        for (Map.Entry<Integer, String> entry : tmap.entrySet())  
         { 
-            context.write(key, result); 
-            count++; 
+  
+            long count = entry.getKey(); 
+            String name = entry.getValue(); 
+            context.write(new Text(name), new LongWritable(count)); 
         } 
     }
 }
